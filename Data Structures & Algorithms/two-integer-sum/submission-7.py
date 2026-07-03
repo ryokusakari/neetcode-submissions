7afbc2f9e4c8ff006d21469981aa34ce48247284
Ryo Kusakari <ryotaro119.k@gmail.com>
2026-07-03 18:41:58 +0100

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        target_memory = defaultdict(int)
+
+        for index, number in enumerate(nums): 
+            if number in target_memory:
+                return [target_memory[number], index]
+            else: 
+                target_memory[target - number] = index
+
+
