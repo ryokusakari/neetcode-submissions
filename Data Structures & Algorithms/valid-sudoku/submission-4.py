009eaec0f0ec6be3aa3446f1d30b39ebdd60f30b
Ryo Kusakari <ryotaro119.k@gmail.com>
2026-08-04 13:06:31 +0100

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        column_map = defaultdict(list)
+        subset_map = defaultdict(list)
+
+        for row in range(9):
+            row_map = [0]*9
+            for column, number in enumerate(board[row]):
+                if number == ".":
+                    continue
+                subset = (row//3, column//3)
+                print(subset)
+                if row_map[int(number) - 1] == 1:
+                    return False
+                elif number in column_map[column]:
+                    return False
+                elif number in subset_map[subset]:
+                    return False
+                else:
+                    row_map[int(number)-1] = 1
+                    column_map[column].append(number)
+                    subset_map[subset].append(number)
+        
+        return True
