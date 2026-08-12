b46f6368b59588a1b6234cfb5384083d13d616ee
Ryo Kusakari <ryotaro119.k@gmail.com>
2026-08-12 11:27:14 +0100

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,34 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+
+        map = [0]*26
+        map_checker = map.copy()
+
+        for s in s1:
+            map[ord(s) - ord("a")] += 1
+        
+        r,l = 0,0
+        while r < len(s1):
+            map_checker[ord(s2[r]) - ord("a")] += 1
+            r += 1
+            
+        while r < len(s2):
+            if map_checker == map: 
+                return True
+            else: 
+                map_checker[ord(s2[r]) - ord("a")] += 1
+                map_checker[ord(s2[l]) - ord("a")] -= 1
+                r += 1
+                l += 1
+        
+        if map_checker == map:
+            return True 
+        else:
+            return False
+            
+
+            
+
+            
